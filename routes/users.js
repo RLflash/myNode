ea1b602a483e5be14d73998a1cf23f04523a2f79
RLflash <2039400885@qq.com>
2019-03-07 10:53:19 +0800

    Add demo

---
+++ routes/users.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var http = require('http');
+/* GET users listing. */
+router.get('/:id', function(req, res, next) {
+	var body=req;
+	res.json({error:req.url}) //{"error":{"id":"2323"}}
+//  var options = { 
+//  	host : '120.24.168.72',    
+//  	port : 80,      
+//  	path : '',  
+//  	method : "GET", 
+//  	
+//  };
+//	var request = http.request(options,function(data){ 
+//		data.setEncoding('utf8'),
+//		data.on('data',function(chunk){
+//			 res.render('users',JSON.parse(chunk));
+//			
+//			//res.json({error:JSON.parse(chunk)})
+//		});  
+//		
+//	});     
+//	
+//	request.end();
+	
+  // res.json({error : "Data Error!1"})
+});
+
+
+module.exports = router;
